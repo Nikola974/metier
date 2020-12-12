ac4cff878d5ab81216295b0e7a004eaf178d6ead
Nikola974 <75907874+Nikola974@users.noreply.github.com>
2020-12-12 20:58:43 +0200

    Add files via upload

--- metier.sql
+++ metier.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2020 at 06:51 PM
+-- Generation Time: Dec 12, 2020 at 07:55 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -28,18 +28,74 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `accounts` (
-  `name` varchar(60) NOT NULL,
-  `fname` varchar(60) NOT NULL,
-  `email` varchar(60) NOT NULL,
-  `password` varchar(8) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL COMMENT 'Име',
+  `fname` varchar(60) NOT NULL COMMENT 'Фамилия',
+  `email` varchar(60) NOT NULL COMMENT 'Email',
+  `username` varchar(60) NOT NULL COMMENT 'Потребителско име',
+  `password` varchar(200) NOT NULL COMMENT 'Парола',
+  `CV` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`name`, `fname`, `email`, `password`) VALUES
-('Petar', 'Petrov', 'Asdasdasd@mail', '12345');
+INSERT INTO `accounts` (`id`, `name`, `fname`, `email`, `username`, `password`, `CV`) VALUES
+(4, 'Иван', 'Иванов', 'Иван@abv.bg', 'Vanko', '$2y$10$wgTdFDgAxZxSBqJPqqovzuk42vISDAFujiTA09O.YNZaSEoT0p7OS', ''),
+(5, 'Петър', 'Петров', 'Петър@abv.bg', 'Petio', '$2y$10$yxuukbPGaVr947Fho9Aok.DARrq/cQNrTqw9rhZwbbBCOlpqRZAty', 'JVBERi0xLjMNCiXi48/TDQoNCjEgMCBvYmoNCjw8DQovVHlwZSAvQ2F0YWxvZw0KL091dGxpbmVzIDIgMCBSDQovUGFnZXMgMyAwIFINCj4+DQplbmRvYmoNCg0KMiAwIG9iag0KPDwNCi9UeXBlIC9PdXRsaW5lcw0KL0NvdW50IDANCj4+DQplbmRvYmoNCg0KMyAwIG9iag0KPDwNCi9UeXBlIC9QYWdlcw0KL0NvdW50IDINCi9LaWRzIFsgNCAwIFIgNiAwIFIgXSANCj4+DQplbmRvYmoNCg0KNCAwIG9iag0KPDwNCi9UeXBlIC9QYWdlDQovUGFyZW50IDMgMCBSDQovUmVzb3VyY2VzIDw8DQovRm9udCA8PA0KL0YxIDkgMCBSIA0KPj4NCi9Qcm9jU2V0IDggMCBSDQo+Pg0KL01lZGlhQm94IFswIDAgNjEyLjAwMDAgNzkyLjAwMDBdDQovQ29udGVudHMgNSAwIFINCj4+DQplbmRvYmoNCg0KNSAwIG9iag0KPDwgL0xlbmd0aCAxMDc0ID4+DQpzdHJlYW0NCjIgSg0KQlQNCjAgMCAwIHJnDQovRjEgMDAyNyBUZg0KNTcuMzc1MCA3MjIuMjgwMCBUZA0KKCBBIFNpbXBsZSBQREYgRmlsZSApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY4OC42MDgwIFRkDQooIFRoaXMgaXMgYSBzbWFsbCBkZW1vbnN0cmF0aW9uIC5wZGYgZmlsZSAtICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjY0LjcwNDAgVGQNCigganVzdCBmb3IgdXNlIGluIHRoZSBWaXJ0dWFsIE1lY2hhbmljcyB0dXRvcmlhbHMuIE1vcmUgdGV4dC4gQW5kIG1vcmUgKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NTIuNzUyMCBUZA0KKCB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDYyOC44NDgwIFRkDQooIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjE2Ljg5NjAgVGQNCiggdGV4dC4gQW5kIG1vcmUgdGV4dC4gQm9yaW5nLCB6enp6ei4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjA0Ljk0NDAgVGQNCiggbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDU5Mi45OTIwIFRkDQooIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNTY5LjA4ODAgVGQNCiggQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA1NTcuMTM2MCBUZA0KKCB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBFdmVuIG1vcmUuIENvbnRpbnVlZCBvbiBwYWdlIDIgLi4uKSBUag0KRVQNCmVuZHN0cmVhbQ0KZW5kb2JqDQoNCjYgMCBvYmoNCjw8DQovVHlwZSAvUGFnZQ0KL1BhcmVudCAzIDAgUg0KL1Jlc291cmNlcyA8PA0KL0ZvbnQgPDwNCi9GMSA5IDAgUiANCj4+DQovUHJvY1NldCA4IDAgUg0KPj4NCi9NZWRpYUJveCBbMCAwIDYxMi4wMDAwIDc5Mi4wMDAwXQ0KL0NvbnRlbnRzIDcgMCBSDQo+Pg0KZW5kb2JqDQoNCjcgMCBvYmoNCjw8IC9MZW5ndGggNjc2ID4+DQpzdHJlYW0NCjIgSg0KQlQNCjAgMCAwIHJnDQovRjEgMDAyNyBUZg0KNTcuMzc1MCA3MjIuMjgwMCBUZA0KKCBTaW1wbGUgUERGIEZpbGUgMiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY4OC42MDgwIFRkDQooIC4uLmNvbnRpbnVlZCBmcm9tIHBhZ2UgMS4gWWV0IG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NzYuNjU2MCBUZA0KKCBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY2NC43MDQwIFRkDQooIHRleHQuIE9oLCBob3cgYm9yaW5nIHR5cGluZyB0aGlzIHN0dWZmLiBCdXQgbm90IGFzIGJvcmluZyBhcyB3YXRjaGluZyApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY1Mi43NTIwIFRkDQooIHBhaW50IGRyeS4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NDAuODAwMCBUZA0KKCBCb3JpbmcuICBNb3JlLCBhIGxpdHRsZSBtb3JlIHRleHQuIFRoZSBlbmQsIGFuZCBqdXN0IGFzIHdlbGwuICkgVGoNCkVUDQplbmRzdHJlYW0NCmVuZG9iag0KDQo4IDAgb2JqDQpbL1BERiAvVGV4dF0NCmVuZG9iag0KDQo5IDAgb2JqDQo8PA0KL1R5cGUgL0ZvbnQNCi9TdWJ0eXBlIC9UeXBlMQ0KL05hbWUgL0YxDQovQmFzZUZvbnQgL0hlbHZldGljYQ0KL0VuY29kaW5nIC9XaW5BbnNpRW5jb2RpbmcNCj4+DQplbmRvYmoNCg0KMTAgMCBvYmoNCjw8DQovQ3JlYXRvciAoUmF2ZSBcKGh0dHA6Ly93d3cubmV2cm9uYS5jb20vcmF2ZVwpKQ0KL1Byb2R1Y2VyIChOZXZyb25hIERlc2lnbnMpDQovQ3JlYXRpb25EYXRlIChEOjIwMDYwMzAxMDcyODI2KQ0KPj4NCmVuZG9iag0KDQp4cmVmDQowIDExDQowMDAwMDAwMDAwIDY1NTM1IGYNCjAwMDAwMDAwMTkgMDAwMDAgbg0KMDAwMDAwMDA5MyAwMDAwMCBuDQowMDAwMDAwMTQ3IDAwMDAwIG4NCjAwMDAwMDAyMjIgMDAwMDAgbg0KMDAwMDAwMDM5MCAwMDAwMCBuDQowMDAwMDAxNTIyIDAwMDAwIG4NCjAwMDAwMDE2OTAgMDAwMDAgbg0KMDAwMDAwMjQyMyAwMDAwMCBuDQowMDAwMDAyNDU2IDAwMDAwIG4NCjAwMDAwMDI1NzQgMDAwMDAgbg0KDQp0cmFpbGVyDQo8PA0KL1NpemUgMTENCi9Sb290IDEgMCBSDQovSW5mbyAxMCAwIFINCj4+DQoNCnN0YXJ0eHJlZg0KMjcxNA0KJSVFT0YNCg=='),
+(10, 'Stoyan', 'Стоянов', 'StoyanStoyanov@gmail.com', 'Stoyan', '$2y$10$0p9TgXKDfLfqOR4zpVriYu1RiyZ.lleD2SjiOJa1vFDGhlV8Ye7.e', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applications`
+--
+
+CREATE TABLE `applications` (
+  `id` int(11) NOT NULL,
+  `offer_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `applications`
+--
+
+INSERT INTO `applications` (`id`, `offer_id`, `user_id`) VALUES
+(1, 1, 4),
+(2, 1, 5),
+(3, 5, 5),
+(4, 6, 5),
+(5, 5, 4),
+(6, 5, 5),
+(7, 6, 5),
+(8, 5, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE `city` (
+  `id` int(11) NOT NULL,
+  `city` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`id`, `city`) VALUES
+(1, 'София'),
+(2, 'Варна'),
+(3, 'Пловдив'),
+(4, 'Бургас'),
+(5, 'Стара Загора'),
+(6, 'Русе'),
+(7, 'Плевен'),
+(8, 'Велико Търново');
 
 -- --------------------------------------------------------
 
@@ -48,14 +104,27 @@ INSERT INTO `accounts` (`name`, `fname`, `email`, `password`) VALUES
 --
 
 CREATE TABLE `offers` (
+  `id` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
   `firmname` varchar(60) NOT NULL COMMENT 'Име на фирмата',
   `bulstat` int(9) NOT NULL COMMENT 'Булстат',
   `address` varchar(60) NOT NULL COMMENT 'Адрес',
-  `sector` varchar(40) NOT NULL COMMENT 'Бизнес сектор',
-  `city` varchar(20) NOT NULL COMMENT 'Град',
+  `id_sector` int(11) NOT NULL,
+  `id_city` int(11) NOT NULL COMMENT 'id на градове',
   `position` varchar(30) NOT NULL COMMENT 'Длъжност',
   `conditions` varchar(1000) NOT NULL COMMENT 'Условия'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `offers`
+--
+
+INSERT INTO `offers` (`id`, `created_by`, `firmname`, `bulstat`, `address`, `id_sector`, `id_city`, `position`, `conditions`) VALUES
+(1, 4, 'Coca Cola', 123456786, 'бул. „8-ми Приморски полк“ 202, 9010 ж.к. Бриз, София', 2, 1, 'Търговски представител', 'Заплата Работно време Трудов стаж Изисквания'),
+(2, 4, 'Wizair', 123456781, 'бул. „8-ми Приморски полк“ 202, 9010 ж.к. Бриз, Варна', 4, 2, 'Стюард/Стюардеса', 'Заплата Работно време Трудов стаж Изисквания'),
+(3, 4, 'Happy', 123456789, 'бул. „8-ми Приморски полк“ 202, 9010 ж.к. Бриз, Стара Загора', 3, 5, 'Готвач', 'Заплата  Работно време  Трудов стаж  Изисквания'),
+(5, 5, 'Sutherland', 123456741, 'бул. „България“ 202, 9010 ж.к. Бриз, София', 1, 1, 'Системен администратор', 'Заплата Работно време Трудов стаж Изисквания'),
+(6, 4, 'Orange', 123432151, 'бул. „Цар Освободител“ 11, 9002 София', 2, 1, 'Продавач консултант', 'Заплата Работно време Трудов стаж Изисквания');
 
 -- --------------------------------------------------------
 
@@ -64,8 +133,8 @@ CREATE TABLE `offers` (
 --
 
 CREATE TABLE `sector` (
-  `id` int(100) NOT NULL,
-  `sector` varchar(200) NOT NULL
+  `id` int(11) NOT NULL,
+  `sector` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -74,7 +143,9 @@ CREATE TABLE `sector` (
 
 INSERT INTO `sector` (`id`, `sector`) VALUES
 (1, 'ИТ'),
-(2, 'Търговия и продажби');
+(2, 'Търговия и продажби'),
+(3, 'Ресторантьорство и Хотелиерство'),
+(4, 'Авиация');
 
 --
 -- Indexes for dumped tables
@@ -84,19 +155,71 @@ INSERT INTO `sector` (`id`, `sector`) VALUES
 -- Indexes for table `accounts`
 --
 ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `applications`
+--
+ALTER TABLE `applications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `offer_id` (`offer_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `offers`
 --
 ALTER TABLE `offers`
-  ADD PRIMARY KEY (`bulstat`);
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD UNIQUE KEY `bulstat` (`bulstat`) USING BTREE,
+  ADD KEY `id_citys` (`id_city`),
+  ADD KEY `id_sector` (`id_sector`);
 
 --
 -- Indexes for table `sector`
 --
 ALTER TABLE `sector`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `applications`
+--
+ALTER TABLE `applications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `offers`
+--
+ALTER TABLE `offers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `sector`
+--
+ALTER TABLE `sector`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
